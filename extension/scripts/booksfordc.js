6c2d4b4b77677dbb963406507034ff004a6c979f
evonfriedland <emanuelfeld@gmail.com>
2015-03-04 18:43:37 -0500

    Switch span to div; account for holds
    For books: if none available and there is a hold list, present hold
    number instead of available number
    For ebooks: if there is a waiting list, present hold number
    
    Fixes #11

--- extension/scripts/booksfordc.js
+++ extension/scripts/booksfordc.js
@@ -11,10 +11,9 @@ if (/amazon\.com$/.test(document.domain)) {
 		var title = $('#btAsinTitle').text().replace(/\(.*\)/g,"").replace(/\[.*\]/,""),
 			author = $('.contributorNameTrigger').text();
 		isEBook = true;
-		console.log(0);
 
 	} else if ($('#productTitle').length) {
-		console.log(1);
+
 		var title = $('#productTitle').text().replace(/\(.*\)/g,"").replace(),
 			author = $('.a-link-normal.contributorNameID:first').text(),
 			isbn13 = $("#productDetailsTable .content li:contains('ISBN-13:')").text(),
@@ -22,10 +21,6 @@ if (/amazon\.com$/.test(document.domain)) {
 		isBook = true;
 
 	} 
-
-	console.log(3);
-	//URLs
-
 
 	if (isEBook == true| isBook == true){
 	
@@ -35,17 +30,16 @@ if (/amazon\.com$/.test(document.domain)) {
 		var isbnURL = base + isbn;
 		//URL to search catalog by title and author for books
 		var taURL = base+encodeURIComponent(title+" "+author).replace(/'/g, "%27")+"&qf=FORMAT%09Bibliographic+Format%09BOOK%09Book";
-		//"&te=&lm=BOOKS";
 		//URL to search catalog for ebooks
 		var ebookURL = base+"TITLE%3D"+encodeURIComponent(title+" ").replace(/'/g, "%27")+"&qu=AUTHOR%3D"+encodeURIComponent(author).replace(/'/g, "%27")+"&qf=FORMAT%09Bibliographic+Format%09E_BOOK%09eBook";
 		//URL to request library purchase
 		var purchaseURL = "http://citycat.dclibrary.org/uhtbin/cgisirsi/x/ML-KING/x/63/1100/X";
 
-		console.log(4);
+		//Determine where to place information on page and run search
 		if (isEBook == true){
-			console.log(5);
+
 			if ($('div.kicsBoxContents').length){
-				console.log(6);
+
 		        var container = $('div.kicsBoxContents');    
 			    container.before("<div id='dcpl_digital'><div id='dcpl_title'>DCPL Search</div> <div id='category'>Library Catalog</div> <div id='book'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div>  <div id='category'>Digital Catalog</div> <div id='digital'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div> </div>");
 		    	var modify_digital = $("div#digital");
@@ -56,22 +50,22 @@ if (/amazon\.com$/.test(document.domain)) {
 		    } 	
 
 		} else {
-			console.log(7);
+
 			if ($('div.a-box.rbbSection.selected.dp-accordion-active').length){
-				console.log(8);
+
 		        var container = $('div.a-box.rbbSection.selected.dp-accordion-active');    
-			    container.prepend("<div id='dcpl' class='a-box'><span id='dcpl_title'>DCPL Search</span> <br> <span id='category'>Library Catalog</span> <br> <span id='book'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </span>  <br> <strong>Digital Catalog</strong> <br> <span id='digital'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </span> </div>");
-		    	var modify_digital = $("span#digital");
-			    var modify_book = $("span#book");
+			    container.prepend("<div id='dcpl' class='a-box'><div id='dcpl_title'>DCPL Search</div> <div id='category'>Library Catalog</div> <div id='book'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div> <div id='category'>Digital Catalog</div> <div id='digital'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div> </div>");
+		    	var modify_digital = $("div#digital");
+			    var modify_book = $("div#book");
 
 				searchCatalog(modify_book,modify_digital,isbnURL,taURL,ebookURL,purchaseURL,isBook,isEBook);
 
-			} else if($('div#unqualifiedBuyBox').length){
-				console.log(9);    
+			} else if ($('div#unqualifiedBuyBox').length){
+
 		        var container = $('div#unqualifiedBuyBox');
-			    container.prepend("<div id='dcpl' class='a-box'><span id='dcpl_title'>DCPL Search</span> <br> <span id='category'>Library Catalog</span> <br> <span id='book'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </span>  <br> <strong>Digital Catalog</strong> <br> <span id='digital'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </span> </div>");
-		    	var modify_digital = $("span#digital");
-			    var modify_book = $("span#book");
+			    container.prepend("<div id='dcpl' class='a-box'><div id='dcpl_title'>DCPL Search</div> <div id='category'>Library Catalog</div> <div id='book'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div> <div id='category'>Digital Catalog</div> <div id='digital'>Searching catalog <img src='"+chrome.extension.getURL('assets/ajax-loader.gif')+"'> </div> </div>");
+		    	var modify_digital = $("div#digital");
+			    var modify_book = $("div#book");
 	
 				searchCatalog(modify_book,modify_digital,isbnURL,taURL,ebookURL,purchaseURL,isBook,isEBook);
 
@@ -80,6 +74,8 @@ if (/amazon\.com$/.test(document.domain)) {
 	}
 
 	function searchCatalog(modify_book,modify_digital,isbnURL,taURL,ebookURL,purchaseURL,isBook,isEBook) {
+
+			//search by ISBN (or title/author URL if on Amazon ebook page and don't know ISBN)
 	     	$.get(isbnURL,
 		        function(data){
 		            var oneline = $(data).text().replace(/\n/g,"");
@@ -87,11 +83,26 @@ if (/amazon\.com$/.test(document.domain)) {
 			      			var availabilityJSON = JSON.parse(oneline.replace(/.*parseDetailAvailabilityJSON\((.+?)\)\;.*/,"$1"));
 			            	var available = availabilityJSON['totalAvailable'].toString();
 			            	var total = availabilityJSON['copies'][0].match(/(\d+)$/)[1];
-			            	if(available.match(/^[0-9]+$/)!=null && total.match(/^1$/)!=null){
-			                	modify_book.html("Book located <br> <a id='results' href = '" + isbnURL + "'>"+total+" Copy ("+available+" Available)</a>");
+			            	var wait = availabilityJSON['holdCounts'][0].match(/(\d+)$/)[1];
+
+					        if (total.match(/^1$/)!=null){
+				            	var total_statement = total + " copy";
+				            } else if (total.match(/^[0-9]+$/)!=null){
+				            	var total_statement = total + " copies";	            	
+				            }
+
+				            if (wait.match(/^1$/)!=null){
+				            	var wait_statement = wait + " patron waiting"
+				            } else if (wait.match(/^[0-9]+$/)!=null) {
+				            	var wait_statement = wait + " patrons waiting"	            	
+				            }
+
+			            	if (wait.match(/^[0-9]+$/)!=null && wait.match(/^0$/)==null && available.match(/^0$/)!=null && total.match(/^[0-9]+$/)!=null){
+			                	modify_book.html("<a id='results' href = '" + isbnURL + "'> Book located </a> <br>"+total_statement+" ("+wait_statement+")");
 			            	} else {
-			                	modify_book.html("Book located <br> <a id='results' href = '" + isbnURL + "'>"+total+" Copies ("+available+" Available)</a>");
+			                	modify_book.html("<a id='results' href = '" + isbnURL + "'> Book located </a> <br>"+total_statement+" ("+available+" available)");
 			            	}
+
 		            	} catch (e) {
 			            	if (isEBook==true){
 			            		if (oneline.match("This search returned no results.")!=null){
@@ -106,12 +117,13 @@ if (/amazon\.com$/.test(document.domain)) {
 		        }
 		    );
 
+			//Determine Overdrive ID to search Overdrive catalog
 			$.get(ebookURL,
 		        function(data){
 		            var oneline = $(data).text().replace(/\n/g,""),
 		            	overdrive_id = oneline.replace(/.*fOVERDRIVE\:(.+?)\$.*/,"$1"),
 		            	overdriveURL = "http://overdrive.dclibrary.org/ContentDetails.htm?id="+overdrive_id;
-		            if(overdrive_id.length>100){
+		            if (overdrive_id.length>100){
 	                	modify_digital.html("E-book not located <br> <a id='results' href = 'http://overdrive.dclibrary.org/'>Search manually</a>");            		
 		            } else {
 			            overdriveSearch(modify_digital,overdriveURL,purchaseURL);		            	
@@ -121,7 +133,8 @@ if (/amazon\.com$/.test(document.domain)) {
 	}
 
 	function taSearch(modify_book,taURL,purchaseURL){
-			//load and format data from catalog search
+
+			//Search library catalog by title/author
 	     	$.get(taURL,
 	        function(data){
 	            var oneline = $(data).text().replace(/\n/g,"");
@@ -129,11 +142,26 @@ if (/amazon\.com$/.test(document.domain)) {
 		      			var book_json = JSON.parse(oneline.replace(/.*parseDetailAvailabilityJSON\((.+?)\)\;.*/,"$1"));
 		            	var available = book_json['totalAvailable'].toString();
 		            	var total = book_json['copies'][0].match(/(\d+)$/)[1];
-		            	if(available.match(/^[0-9]+$/)!=null && total.match(/^1$/)!=null){
-		                	modify_book.html("Book located <br> <a id='results' href = '" + taURL + "'>"+total+" Copy ("+available+" Available)</a>");
-		            	} else {
-		                	modify_book.html("Book located <br> <a id='results' href = '" + taURL + "'>"+total+" Copies ("+available+" Available)</a>");
+		            	var wait = availabilityJSON['holdCounts'][0].match(/(\d+)$/)[1];
+
+				        if (total.match(/^1$/)!=null){
+			            	var total_statement = total + " copy";
+			            } else if (total.match(/^[0-9]+$/)!=null){
+			            	var total_statement = total + " copies";	            	
+			            }
+
+			            if (wait.match(/^1$/)!=null){
+			            	var wait_statement = wait + " patron waiting"
+			            } else if (wait.match(/^[0-9]+$/)!=null) {
+			            	var wait_statement = wait + " patrons waiting"	            	
+			            }
+
+		            	if (holds.match(/^[0-9]+$/)!=null && holds.match(/^0$/)==null && available.match(/^0$/)!=null && total.match(/^[0-9]+$/)!=null){
+		                	modify_book.html("<a id='results' href = '" + isbnURL + "'> Book located </a> <br>"+total_statement+" ("+wait_statement+")");
+		                } else {
+		                	modify_book.html("Book located <br> <a id='results' href = '" + taURL + "'>"+total_statement+" copies ("+available+" available)</a>");
 		            	}
+
 	            	} catch (e) {
 						if (oneline.match("This search returned no results.")!=null){
 			                modify_book.html("Book not located <br> <a id='results' href = '" + taURL + "'>Search manually</a> <br> <a id='results' href = '" + purchaseURL + "'>Request purchase</a>");	            	
@@ -146,17 +174,32 @@ if (/amazon\.com$/.test(document.domain)) {
 	}
 
 	function overdriveSearch(modify_digital,overdriveURL,purchaseURL){
-		console.log(modify_digital);
-		console.log(overdriveURL);
+
+		//Search Overdrive catalog
 		$.get(overdriveURL,
 			function(data){
 				var oneline = $(data).text().replace(/\n/g,"");
-	            var available = oneline.replace(/.*Available:(\d+).*/,"$1");
-	            var total = oneline.replace(/.*Library copies:(\d+).*/,"$1");
-            	if(available.match(/^[0-9]+$/)!=null && total.match(/^1$/)!=null){
-                	modify_digital.html("E-book located <br> <a id='results' href = '" + overdriveURL + "'>"+total+" Copy ("+available+" Available)</a>");
-            	} else if(available.match(/^[0-9]+$/)!=null && total.match(/^[0-9]+$/)!=null) {
-                	modify_digital.html("E-book located <br> <a id='results' href = '" + overdriveURL + "'>"+total+" Copies ("+available+" Available)</a>");
+				var availability = oneline.replace(/.*Copies-Available:(\d+)Library copies:(\d+)var deNumWaiting = (\d+?)\;.*/,"$1,$2,$3");
+	            var available = availability.split(',')[0];
+	            var total = availability.split(',')[1];
+	            var wait = availability.split(',')[2];
+
+	            if (total.match(/^1$/)!=null){
+	            	var total_statement = total + " copy";
+	            } else if (total.match(/^[0-9]+$/)!=null){
+	            	var total_statement = total + " copies";	            	
+	            }
+
+	            if (wait.match(/^1$/)!=null){
+	            	var wait_statement = wait + " patron waiting"
+	            } else if (wait.match(/^[0-9]+$/)!=null) {
+	            	var wait_statement = wait + " patrons waiting"	            	
+	            }
+
+        		if (wait.match(/^[0-9]+$/)!=null && wait.match(/^0$/)==null && total.match(/^[0-9]+$/)!=null){
+            		modify_digital.html("<a id='results' href = '" + overdriveURL + "'> E-book located </a> <br>"+total_statement+" ("+wait_statement+")</a>");
+        		} else if (available.match(/^[0-9]+$/)!=null && total.match(/^[0-9]+$/)!=null) {
+                	modify_digital.html("<a id='results' href = '" + overdriveURL + "'> E-book located </a> <br> "+total_statement+" ("+available+" available)</a>");
             	} else {
                 	modify_digital.html("E-book not located <br> <a id='results' href = 'http://overdrive.dclibrary.org/'>Search manually</a>");            		
             	}
