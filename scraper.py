76fd75e96601139107fde71590c7d4e4a2d4b146
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2015-03-18 17:28:08 -0400

    Create scraper.py
    Scraper for Freegal music database

---
+++ scraper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import scraperwiki, re, lxml.html, time
+
+def clean(thing):
+    thing = re.sub(r'^\s*(.*?)\s*$',r'\1',thing)
+    thing = re.sub(r'\n',r' ',thing)
+    return thing
+
+for i in xrange(1,100):
+    print i
+    page = 'http://dclibrary.freegalmusic.com/genres/ajax_view_pagination/page:'+str(i)+'/QWxs/All'
+    try:
+        html=scraperwiki.scrape(page)
+        root = lxml.html.fromstring(html)
+        for x in root.cssselect("body li a"):
+            music = {
+                "artist": clean(x.text_content()),
+                "url": "http://dclibrary.freegalmusic.com"+clean(x.xpath('@href')[0])            
+                }
+            scraperwiki.sql.save(unique_keys=['url'], data=music)
+    except:
+        print "Not scraped: "+page
+    time.sleep(60)
