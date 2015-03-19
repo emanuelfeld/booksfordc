9371903b3109557936b31efd1cef2f6cbe8265cb
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2015-03-18 20:20:34 -0400

    Update scraper.py
    Speed improvement by dividing alphabetically

--- scraper.py
+++ scraper.py
@@ -7,18 +7,25 @@ def clean(thing):
     thing = re.sub(r'\n',r' ',thing)
     return thing
 
-for i in xrange(1,100):
-    print i
-    page = 'http://dclibrary.freegalmusic.com/genres/ajax_view_pagination/page:'+str(i)+'/QWxs/All'
-    try:
-        html=scraperwiki.scrape(page)
-        root = lxml.html.fromstring(html)
-        for x in root.cssselect("body li a"):
-            music = {
-                "artist": clean(x.text_content()),
-                "url": "http://dclibrary.freegalmusic.com"+clean(x.xpath('@href')[0])            
-                }
-            scraperwiki.sql.save(unique_keys=['url'], data=music)
-    except:
-        print "Not scraped: "+page
-    time.sleep(30)
+alphabet = ['spl','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+for l in alphabet:
+    print l
+    i = 1
+    ok = True
+    while ok==True:
+        page = 'http://dclibrary.freegalmusic.com/genres/ajax_view_pagination/page:'+str(i)+'/QWxs/'+l
+        try:
+            html=scraperwiki.scrape(page)
+            root = lxml.html.fromstring(html)
+            for x in root.cssselect("body li a"):
+                music = {
+                    "artist": clean(x.text_content()),
+                    "url": "http://dclibrary.freegalmusic.com"+clean(x.xpath('@href')[0])            
+                    }
+                scraperwiki.sql.save(unique_keys=['url'], data=music)
+            print i
+            time.sleep(10)
+        except:
+            ok = False
+        i+=1
