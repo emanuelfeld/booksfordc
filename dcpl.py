921c567d0a660e8c14da409b86f4f0769870a614
Emanuel Feld <emanuelfeld@users.noreply.github.com>
2015-08-12 19:43:08 -0400

    Update dcpl.py

--- dcpl.py
+++ dcpl.py
@@ -3,7 +3,7 @@
 
 from twitterbot import TwitterBot
 from datetime import datetime
-import cssselect, logging, lxml.html, os, re, requests, time
+import cssselect, logging, lxml.html, os, re, requests, time, pyopenssl, ndg-httpsclient, pyasn1
 
 logging.basicConfig(level=logging.INFO)
 
