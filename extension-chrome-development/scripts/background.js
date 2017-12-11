746ef1c76ec0ecfec12632008c74c05ad537fb1f
emanuelfeld <emanuelfeld@gmail.com>
2017-12-11 14:14:03 -0500

    cleanup

---
+++ extension-chrome-development/scripts/background.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict'
+  
+  if (!!window.chrome) {
+    window.browser = window.chrome
+  } else {
+    window.browser = browser
+  }
+
+  var handleOptionsClick = function () {
+    try {
+      window.browser.runtime.openOptionsPage()
+    } catch (e) {
+      window.open(window.browser.runtime.getURL('options.html'))
+    }
+  }
+
+  window.browser.runtime.onMessage.addListener(handleOptionsClick)
+
+  window.browser.runtime.onInstalled.addListener(function (details) {
+    // var version = window.browser.runtime.getManifest().version
+    // if (details.reason === 'install') {
+    //   window.browser.tabs.create({
+    //     'url': `https://booksfordc.org/welcome-chrome?utm_source=chrome&utm_campaign=install&version=` + version
+    //   })
+    //   window.browser.tabs.create({
+    //     'url': `https://booksfordc.org/update?utm_source=chrome&utm_campaign=update&version=` + version
+    //   })      
+    // } else if (details.reason === 'update') {
+    //   window.browser.tabs.create({
+    //     'url': `https://booksfordc.org/update?utm_source=chrome&utm_campaign=update&version=` + version
+    //   })      
+    // }
+  })
+
+  window.browser.runtime.setUninstallURL(`http://bit.ly/bfdc-chrome`)  
+})()
