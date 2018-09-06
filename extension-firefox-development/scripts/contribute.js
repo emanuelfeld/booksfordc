b900f6fa1e5f4a25854a3fc167121e03ec7d005c
emanuelfeld <emanuelfeld@gmail.com>
2018-09-06 10:35:43 -0400

    http -> https

--- extension-firefox-development/scripts/contribute.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    document.getElementById('contribute-amount-submit').addEventListener('click', function () {
-        let amount = document.getElementById('contribute-amount-input').value
-        window.open(`https://booksfordc.herokuapp.com/contribute?amount=${amount}&utm_source=firefox&utm_campaign=options`)
-    })
-})()
